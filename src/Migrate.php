37af9ea4e544826c308edc001851c98b455ca1f2
Piotr Lewandowski <plew1961@gmail.com>
2026-02-06 06:03:38 +0100

    09Work

--- src/Migrate.php
+++ src/Migrate.php
@@ -8,9 +8,9 @@ class Migrate
     {
         if (empty($dir)) return false;
         date_default_timezone_set('Europe/Warsaw');
-        $handle = fopen($dir . '/' . 'Migrate' . date('YmdHis') . '.php', 'w');
+        $handle = fopen($dir . '/' . 'Migrate' . date('YmdHis') . '.sql', 'w');
         if ($handle === false) return false;
-        if (fwrite($handle, '<?php' . PHP_EOL) === false) return false;
+        if (fwrite($handle, '# Edit your query, there may be several, each at the end \';\'' . PHP_EOL) === false) return false;
         return fclose($handle);
     }
 }
