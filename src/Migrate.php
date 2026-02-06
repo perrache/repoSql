1ca3d5bb4c5148c9507d7fe58db967e07d2b7df8
Piotr Lewandowski <plew1961@gmail.com>
2026-02-06 05:22:35 +0100

    06Work

--- src/Migrate.php
+++ src/Migrate.php
@@ -4,9 +4,13 @@ namespace Perrache\Reposql;
 
 class Migrate
 {
-    public function createFile(string $dir)
+    public function createFile(string $dir): bool
     {
+        if (empty($dir)) return false;
         date_default_timezone_set('Europe/Warsaw');
-        return 'Migrate' . date('YmdHis');
+        $handle = fopen($dir . '/' . 'Migrate' . date('YmdHis'), 'w');
+        if ($handle === false) return false;
+        if (fwrite($handle, '<?php' . PHP_EOL) === false) return false;
+        return fclose($handle);
     }
 }
